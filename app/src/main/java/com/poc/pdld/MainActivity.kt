4c109a081ba43ca1635dd446844d77f9c1f30c70
Aayushi <aayushitiwari21@navgurukul.org>
2025-02-12 05:14:20 +0530

    current updated

--- app/src/main/java/com/poc/pdld/MainActivity.kt
+++ app/src/main/java/com/poc/pdld/MainActivity.kt
@@ -37,7 +37,7 @@ and posted on the server database when the internet connection will be their
 
 1. Offline Data Submission - Room (Data is submitting and deleting for now offline mode)
 2. Automatic Sync When Internet is Available - Firestore - Almost done just add your data locally when internet
-is available it updates on the server database
+is available it updates on the server database + fetch the data from remote server for other user
 3. Data Caching When User is Inactive from screen - AutoSaving
 4. Offline Search Functionality - Search Through Database (Query from Room + Firestore)
 
